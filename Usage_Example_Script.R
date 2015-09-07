704e4d9a06b9fe1fbad2c9835c32901b4b78c753
vertesy <a.vertesy@hubrecht.eu>
2015-09-07 17:15:56 +0200

    Branch with Github compatible reporting

--- Usage_Example_Script.R
+++ Usage_Example_Script.R
@@ -1,15 +1,13 @@
 ######################################################################
-#  Example usage
+#  Usage_Example_Script
 ######################################################################
-# source ("/Users/abelvertesy/MarkDownLogs/ .R")
-
-Var1 =233
+# source ("/Users/abelvertesy/MarkDownLogs/Usage_Example_Script.R")
 
 Par1 =33
 Par3 =3
 
-OutDir = create_set_OutDir("/Users/abelvertesy/MarkDownLogs/YourOutput");
-setup_logging_markdown ("ExampleLog", append = F)
+OutDir = create_set_OutDir("/Users/abelvertesy/MarkDownLogs/Usage_Example_Script");
+setup_logging_markdown ("Usage_Example_Script.R", append = F)
 
 # save your script params
 log_settings_MarkDown (Par1, Par3)
@@ -25,26 +23,26 @@ llprint("# Start formatting: This is header 1")
 llprint("## This is header 2")
 llprint(" - This is a list")
 llprint(" - that goes on...")
-llogit("")
-llprint('[This link shows you more of the MarkDown Syntax]("https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet")')
+llprint(' - [This link shows you more of the MarkDown Syntax]("https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet")')
 
 
 
 llprint("## Start integrating your plotted results into the story")
 
-Data = rnorm(1000)
-
 # create a plot
+Data = rnorm(1000)
 hist(Data)
+llprint("To integrate plots, you need to:")
+llprint("1. create the plot,")
+llprint("- save into a file, ")
+llprint("- and put a link into your report referring to the file.")
+llprint("This is all done at once with my wplot, whist, etc functions.")
 
 # create a plot and save it to the output folder
 whist(Data)
 
 # create a plot and save it, and put it in the report by the "MDLINK=TRUE" parameter
 whist(Data, mdlink = T)
-
-llprint("It inserts 2 links, one for the .pdf version (created) and one for a .png version (that you might wanna create). Simply ignore the broken link")
-llprint(' - If you dont like this style, just remove the png line from function "MarkDown_Img_Logger_PDF_and_PNG" (around line 89).')
 
 
 llprint("I do not have save_and_log plotting functions  for every plot you might want, so create your dream plot and then save it from the active device by: wplot_save_this function")
@@ -53,3 +51,14 @@ llprint("I do not have save_and_log plotting functions  for every plot you might
 heatmap(matrix(Data, ncol=3,nrow = 5))
 hist(Data, add=T)
 wplot_save_this(plotname = "DreamMap", mdlink = T)
+
+llprint("## You can insert code if you wanna")
+
+llprint('`heatmap(matrix(Data, ncol=3,nrow = 5))`')
+llprint('`hist(Data, add=T)`')
+llprint('`wplot_save_this(plotname = "DreamMap", mdlink = T)`')
+
+
+
+# llprint("It inserts 2 links, one for the .pdf version (created) and one for a .png version (that you might wanna create). Simply ignore the broken link")
+# llprint(' - If you dont like this style, just remove the png line from function "MarkDown_Img_Logger_PDF_and_PNG" (around line 89).')
