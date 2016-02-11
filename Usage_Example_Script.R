158345afe6972dc3c21cf220e83b0f880a85d24a
vertesy <a.vertesy@hubrecht.eu>
2016-02-11 21:33:38 +0100

    Major Upgrade and cleanup

--- Usage_Example_Script.R
+++ Usage_Example_Script.R
@@ -1,74 +1,80 @@
 ######################################################################
 #  Usage_Example_Script
 ######################################################################
-<<<<<<< HEAD:Example_Usage.R
-# source ("/Users/abelvertesy/MarkDownLogs/ .R")
-
-
-
-Var1 =233
-=======
 # source ("/Users/abelvertesy/MarkDownLogs/Usage_Example_Script.R")
->>>>>>> GitHub_Report:Usage_Example_Script.R
 
+print("Use MOU or any other Markdown editor to see resutls")
+
+# Setup --------------------
 Par1 =33
 Par3 =3
 
 OutDir = create_set_OutDir("/Users/abelvertesy/MarkDownLogs/Usage_Example_Script");
 setup_logging_markdown ("Usage_Example_Script.R", append = F)
 
-# save your script params
-log_settings_MarkDown (Par1, Par3)
+# Start logging ----------------------------
+llprint("## Start logging")
+llprint("1 The **llprint** command prints your to the terminal, giving immediate feedback.")
+llogit ("- The **llogit** command puts a line in the file, but does not print it in the terminal.")
+llprint("- You can parse sentences from your variables, parameters and text easily:")
+llprint("Par1 (", Par1, ") is bigger than Par3 (", Par3,")." )
+llprint("- You can put source code within backticks if neccesary to interpret results:")
+llprint("A peculiar feature of R is rounding: `round(1.5) = 2`, just like `round(2.5) = 2`.")
 
-# start logging
-llogit("llogit command puts a line in the file, but do not print it ")
-llprint("llprint command prints your to the terminal, giving immediate feedback.")
-llprint("This is useful if you parse sentences:")
-llprint("Par1 (", Par1, ") is bigger than Par3 (", Par3,")" )
 
-# Start formatting
-llprint("# Start formatting: This is header 1")
-llprint("## This is header 2")
+# Markdown Formatting --------------------------------
+llprint("## You already used the handiest formatting commands ...")
+llprint("### This is header 3")
 llprint(" - This is a list")
 llprint(" - that goes on...")
 llprint(' - [This link shows you more of the MarkDown Syntax]("https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet")')
 
+# Table handling ----------------------------------------------------
+llprint("## Table handling")
+llprint("Save your script parameters in the report (you might need it to exactly reproduce a plot a month later)")
+
+log_settings_MarkDown (Par1, Par3)
 
 
-llprint("## Start integrating your plotted results into the story")
+# Create and integrate figures ----------------------------------------------------
+llprint("## Plot, save and integrate (link & display) your results into the story")
 
 # create a plot
-Data = rnorm(1000)
-hist(Data)
+data = rnorm(1000)
+hist(data)
+
 llprint("To integrate plots, you need to:")
 llprint("1. create the plot,")
 llprint("- save into a file, ")
 llprint("- and put a link into your report referring to the file.")
-llprint("This is all done at once with my wplot, whist, etc functions.")
+llprint("***This is all done at once with my wplot, whist, etc functions.***")
 
 # create a plot and save it to the output folder
-whist(Data)
+llprint("The `whist(data)` creates a plot and saves it as pdf.")
+whist(data)
 
-# create a plot and save it, and put it in the report by the "MDLINK=TRUE" parameter
-whist(Data, mdlink = T)
+# create a plot, save it, AND put it in the report by the "MDLINK=TRUE" parameter
+whist(data, mdlink = T)
+llprint("The `whist(data, mdlink = T)` creates a plot and saves it as pdf, and displays.")
 
 
-llprint("I do not have save_and_log plotting functions  for every plot you might want, so create your dream plot and then save it from the active device by: wplot_save_this function")
+llprint("There might be no 'save&log' plotting functions for every kind of plot you might need,
+		so create your dream plot with any R function, and then save it from the active device by the `wplot_save_this()` function!")
 
 # Lets save figures for that there is
-heatmap(matrix(Data, ncol=3,nrow = 5))
-hist(Data, add=T)
+heatmap(matrix(data, ncol=3,nrow = 5))
+hist(data, add=T)
 wplot_save_this(plotname = "DreamMap", mdlink = T)
 
-llprint("## You can insert code if you wanna")
+llprint("## GitHub Integration")
 
-llprint('`heatmap(matrix(Data, ncol=3,nrow = 5))`')
-llprint('`hist(Data, add=T)`')
-llprint('`wplot_save_this(plotname = "DreamMap", mdlink = T)`')
+llprint("To upload and correctly display figures on GitHub, you need")
+llprint(" - a png/jpg version of the pdf ")
+llprint(" - an extra link to that image in the markdown file. ")
+llprint('     - This is already in place, unless you set the "png4Github" parameter to FALSE in the `setup_logging_markdown()` function.')
+llprint("     - *GitHub takes relative path from the wiki folder, MOU takes it from the .md files location*")
+
+llprint("Take a look at the [code](https://github.com/vertesy/MarkDownLogs/blob/master/MarkDownLogger_functions.R) (MarkDownLogger_functions.R) for further explanation. It is very short :-).")
 
 
-
-##
-
-# llprint("It inserts 2 links, one for the .pdf version (created) and one for a .png version (that you might wanna create). Simply ignore the broken link")
-# llprint(' - If you dont like this style, just remove the png line from function "MarkDown_Img_Logger_PDF_and_PNG" (around line 89).')
+llprint("#### You probably can already decide if MarkDownLogs can help you or not. Thanks for reading!")
