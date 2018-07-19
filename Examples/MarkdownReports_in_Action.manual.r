e8117179cc5b1eff809679f94dbed8880bb016d6
vertesy <a.vertesy@hubrecht.eu>
2018-07-19 14:01:13 +0000

    Example script

--- Examples/MarkdownReports_in_Action.manual.r
+++ Examples/MarkdownReports_in_Action.manual.r
@@ -2,24 +2,26 @@
 ## MarkdownReports in Action
 ######################################################################################################
 # source("~/MarkdownReports.wiki/Reports/MarkdownReports_in_Action.r")
+rm(list=ls(all.names = TRUE));
+try.dev.off()
 
 
 
 # Functions ------------------------
-# source ('~/Github_repos/TheCorvinas/R/CodeAndRoll.R')
-source("https://raw.githubusercontent.com/vertesy/TheCorvinas/master/R/CodeAndRoll.R")
+require(MarkdownReports)
+# source("~/Github_repos/MarkdownReports/MarkdownReports/R/MarkdownReports.R")
+# source("https://raw.githubusercontent.com/vertesy/TheCorvinas/master/R/CodeAndRoll.R")
 # library(stringr)
 
 # Setup ------------------------
-setup_MarkdownReports(OutDir = "~/MarkdownReports/Examples/MarkdownReports_in_Action", scriptname =  "MarkdownReports_in_Action.r", append = F)
-# create_set_OutDir("~/Google_Drive/X_react_Data/Abelz/X_react_2016")
+OutDir = "/Users/abelvertesy/Github_repos/MarkdownReports/Examples/MarkdownReports_in_Action"
+setup_MarkdownReports(OutDir=OutDir,, scriptname =  "MarkdownReports_in_Action.r", title = "Snowflakes")
 llprint("_I will show an (imaginary) example workflow on complitely made up data._")
 
-OutDirOrig = OutDir
 
 # Your data ------------------------
 
-SnowflakeSizes_Reykjavik = c(1.21, 1.31, 1.14, 1.3, 1.11, 1.09, 1.22, 1.191, 1.31, 1.16, 1.19, 1.23, 1.29); l(SnowflakeSizes_Reykjavik)
+SnowflakeSizes_Reykjavik = c(1.21, 1.31, 1.14, 1.3, 1.11, 1.09, 1.22, 1.191, 1.31, 1.16, 1.19, 1.23, 1.29); length(SnowflakeSizes_Reykjavik)
 names (SnowflakeSizes_Reykjavik) = c( '09-Jan', '10-Jan', '11-Jan', '12-Jan', '15-Jan', '16-Jan', '17-Jan', '18-Jan', '19-Jan', '20-Jan', '21-Jan', '22-Jan', '23-Jan' )
 
 # Measurement_Bias = 	3*(2+(rnorm(13))^2) # inline_vec.num(iround(Measurement_Bias))
@@ -31,6 +33,7 @@ SnowflakeSizes =  list (
 	"Anchorage" = c(1.3, 1.41, 1.24, 1.6, 1.7, .9)
 )
 
+
 # Parameters ------------------------
 thresholdX = 10 # % bias is accepted
 
@@ -40,28 +43,36 @@ thresholdX = 10 # % bias is accepted
 llprint("### Hey Snowflake collector, welcome back from Reykjavik! How big are the snowflakes over there?")
 
 llprint("Take a look at the raw numbers:")
-MarkDown_Table_writer_NamedVector(SnowflakeSizes_Reykjavik)
-llogit("The code:")
+md.tableWriter.VEC.w.names(SnowflakeSizes_Reykjavik)
+llogit('The code:
+```
+md.tableWriter.VEC.w.names(SnowflakeSizes_Reykjavik)
+```')
 
 
 llprint("Let's visualize them:")
 wbarplot(SnowflakeSizes_Reykjavik, mdlink = T)
-llogit("The code:")
+llogit('The code:
+```
+wbarplot(SnowflakeSizes_Reykjavik, mdlink = T)
+```')
 
 
 llprint("### At first we would like to throw away every  measurement where the measurement bias (reported by your snowflake collecting machine) is above 10%:")
 wbarplot(Measurement_Bias, ylab = "Measurement Bias (%)", hline = thresholdX, filtercol = -1, mdlink = T)
 
 pass = filter_LP(Measurement_Bias, threshold = thresholdX) # report the actual numbers
-llogit("The code:")
+# llogit("The code:")
 
 # --------------------------------------------------------------------------------
 llprint("### Let's see how it compares with snow flakes from other cities?")
 SnowflakeSizes$Reykjavik = SnowflakeSizes_Reykjavik[pass]
 
 wstripchart(SnowflakeSizes, tilted_text = T, mdlink = T)
-llogit("The code:")
-
+llogit('The code:
+```
+wstripchart(SnowflakeSizes, tilted_text = T, mdlink = T)
+```')
 
 # --------------------------------------------------------------------------------
 llprint("### Let's say, we also measured the temperature of the flakes. We can color flakes that had temperature below -10:")
@@ -75,7 +86,17 @@ SnowflakeTemperature = list( c(-13.3, -13.1, -11.4, -15, -15, -6.28, -9.02),
 colore = lapply(SnowflakeTemperature, function(x) (x< -10)+1)
 SnowflakeSizes_colored_by_temp = SnowflakeSizes
 wstripchart_list(SnowflakeSizes_colored_by_temp, tilted_text = T, bg = colore, mdlink = T)
-llogit("The code:")
+llogit('The code:
+```
+SnowflakeTemperature = list( c(-13.3, -13.1, -11.4, -15, -15, -6.28, -9.02),
+							 c(-9.02, -5.98, -10.5, 0.48, 4.56, -16.4),
+       c(-8.76, -12.6, -9.02, -13.2, -13.5, -10.9, -12.2, -11.6, -10.7, -9.27) )
+
+       colore = lapply(SnowflakeTemperature, function(x) (x< -10)+1)
+       SnowflakeSizes_colored_by_temp = SnowflakeSizes
+       wstripchart_list(SnowflakeSizes_colored_by_temp, tilted_text = T, bg = colore, mdlink = T)
+
+```')
 
 Snowflakes = cbind(
 	"Temperature" = unlist(SnowflakeTemperature),
@@ -87,6 +108,7 @@ Mean_Snowflake_Size_and_Temp = cbind(
 	"Size" = unlist(lapply(SnowflakeSizes, mean))
 )
 
+sem <- function(x, na.rm=T) sd(unlist(x), na.rm = na.rm)/sqrt(length(na.omit.strip(as.numeric(x))))  # Calculates the standard error of the mean (SEM) for a numeric vector (it excludes NA-s by default)
 Snowflakes_SEM = cbind(
 	"Temperature" = unlist(lapply(SnowflakeTemperature, sem)),
 	"Size" = unlist(lapply(SnowflakeSizes, sem))
@@ -94,9 +116,40 @@ Snowflakes_SEM = cbind(
 
 llprint("### And let's see how the correlation looks like for snowflakes in each city:")
 wplot(Mean_Snowflake_Size_and_Temp, errorbar = T, upper = Snowflakes_SEM[,"Size"], left = Snowflakes_SEM[,"Temperature"], col =3:5, cex=2)
-legend("topleft", legend = rownames(Mean_Snowflake_Size_and_Temp), fill = 3:5, bty="n")
 
-wplot_save_this(plotname = plotnameLastPlot, mdlink = T)
+legend_=3:5
+names(legend_) = rownames(Mean_Snowflake_Size_and_Temp)
+wlegend( fill_= legend_, poz = 3,bty="n")
+wLinRegression(Mean_Snowflake_Size_and_Temp, lty=3 )
 
 
-llogit("The code:")
+
+llogit('The code:
+```Snowflakes = cbind(
+	"Temperature" = unlist(SnowflakeTemperature),
+       "Size" = unlist(SnowflakeSizes)
+)
+
+Mean_Snowflake_Size_and_Temp = cbind(
+"Temperature" = unlist(lapply(SnowflakeTemperature, mean)),
+"Size" = unlist(lapply(SnowflakeSizes, mean))
+)
+
+sem <- function(x, na.rm=T) sd(unlist(x), na.rm = na.rm)/sqrt(length(na.omit.strip(as.numeric(x))))  # Calculates the standard error of the mean (SEM) for a numeric vector (it excludes NA-s by default)
+Snowflakes_SEM = cbind(
+"Temperature" = unlist(lapply(SnowflakeTemperature, sem)),
+"Size" = unlist(lapply(SnowflakeSizes, sem))
+)
+
+llprint("### And lets see how the correlation looks like for snowflakes in each city:")
+wplot(Mean_Snowflake_Size_and_Temp, errorbar = T, upper = Snowflakes_SEM[,"Size"], left = Snowflakes_SEM[,"Temperature"], col =3:5, cex=2)
+
+legend_=3:5
+names(legend_) = rownames(Mean_Snowflake_Size_and_Temp)
+wlegend( fill_= legend_, poz = 3,bty="n")
+
+# linear regression and correlation coefficient
+wLinRegression(Mean_Snowflake_Size_and_Temp, lty=3 )
+```')
+
+
