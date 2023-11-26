803c6ca3f94c832e8f07c8ed943aafe5883a9855
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 00:25:46 +0100

    deps

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,55 +1,44 @@
+Dependency file generated on Mon Nov 27 00:25:24 2023 
+
+#################################################################################################### 
+MarkdownReports.R
+#################################################################################################### 
 $`c("package:graphics", "package:base")`
 [1] "plot"
 
 $`character(0)`
-[1] "."             "grid.draw"     "grid.newpage"  "na.omit.strip"
-[5] "rich.colors"   "venn.diagram"  "vioplot"       "write_clip"   
+ [1] "."                 "AddTrailingSlash"  "grid.draw"         "grid.newpage"      "RemoveDoubleSlash"
+ [6] "rich.colors"       "session_info"      "venn.diagram"      "vioplot"           "write_clip"       
 
 $`package:base`
- [1] "abs"           "any"           "as.character"  "as.vector"    
- [5] "basename"      "bquote"        "c"             "cbind"        
- [9] "colnames"      "cut"           "date"          "dim"          
-[13] "dir.create"    "dir.exists"    "exists"        "format"       
-[17] "getwd"         "invisible"     "is.data.frame" "is.list"      
-[21] "is.matrix"     "is.null"       "is.numeric"    "is.table"     
-[25] "is.vector"     "lapply"        "length"        "list"         
-[29] "log10"         "make.names"    "matrix"        "max"          
-[33] "mean"          "min"           "missing"       "names"        
-[37] "nchar"         "ncol"          "on.exit"       "options"      
-[41] "paste"         "paste0"        "print"         "range"        
-[45] "rbind"         "rep"           "rm"            "seq"          
-[49] "setwd"         "stopifnot"     "substitute"    "substr"       
-[53] "sum"           "summary"       "Sys.time"      "system"       
-[57] "table"         "try"           "unique"        "unlist"       
-[61] "which"         "write"         "writeLines"   
+ [1] "abs"           "any"           "as.character"  "as.vector"     "basename"      "bquote"        "c"            
+ [8] "cbind"         "colnames"      "cut"           "date"          "dim"           "dir.create"    "dir.exists"   
+[15] "exists"        "format"        "getwd"         "invisible"     "is.data.frame" "is.list"       "is.matrix"    
+[22] "is.null"       "is.numeric"    "is.table"      "is.vector"     "lapply"        "length"        "list"         
+[29] "log10"         "make.names"    "matrix"        "max"           "mean"          "min"           "missing"      
+[36] "names"         "nchar"         "ncol"          "on.exit"       "options"       "paste"         "paste0"       
+[43] "print"         "range"         "rbind"         "rep"           "rm"            "seq"           "setwd"        
+[50] "stopifnot"     "substitute"    "substr"        "sum"           "summary"       "Sys.time"      "system"       
+[57] "table"         "try"           "unique"        "unlist"        "which"         "write"         "writeLines"   
 
 $`package:CodeAndRoll2`
-[1] "cv"        "iround"    "modus"     "translate"
-
-$`package:devtools`
-[1] "session_info"
+[1] "cv"            "iround"        "modus"         "na.omit.strip" "translate"    
 
 $`package:graphics`
- [1] "abline"      "arrows"      "axis"        "Axis"        "barplot"    
- [6] "box"         "boxplot"     "bxp"         "grid"        "hist"       
-[11] "layout"      "lcm"         "legend"      "mtext"       "par"        
-[16] "pie"         "plot.new"    "plot.window" "points"      "rect"       
-[21] "stripchart"  "text"        "title"      
+ [1] "abline"      "arrows"      "axis"        "Axis"        "barplot"     "box"         "boxplot"     "bxp"        
+ [9] "grid"        "hist"        "layout"      "lcm"         "legend"      "mtext"       "par"         "pie"        
+[17] "plot.new"    "plot.window" "points"      "rect"        "stripchart"  "text"        "title"      
 
 $`package:grDevices`
-[1] "colorRampPalette" "dev.copy"         "dev.copy2pdf"     "dev.off"         
-[5] "pdf"             
+[1] "colorRampPalette" "dev.copy"         "dev.copy2pdf"     "dev.off"          "pdf"             
+[6] "png"             
 
 $`package:MarkdownHelpers`
- [1] "filter_HP"               "filter_LP"              
- [3] "filter_MidPass"          "llogit"                 
- [5] "llprint"                 "md.image.linker"        
- [7] "md.LogSettingsFromList"  "stopif"                 
- [9] "try.dev.off"             "unless.specified"       
-[11] "ww.assign_to_global"     "ww.autoPlotName"        
-[13] "ww.md.image.link.parser" "ww.set.mdlink"          
-[15] "ww.set.OutDir"           "ww.set.PlotName"        
-[17] "ww.ttl_field"           
+ [1] "filter_HP"               "filter_LP"               "filter_MidPass"          "llogit"                 
+ [5] "llprint"                 "md.image.linker"         "md.LogSettingsFromList"  "stopif"                 
+ [9] "try.dev.off"             "unless.specified"        "ww.assign_to_global"     "ww.autoPlotName"        
+[13] "ww.FnP_parser"           "ww.md.image.link.parser" "ww.set.mdlink"           "ww.set.OutDir"          
+[17] "ww.set.PlotName"         "ww.ttl_field"           
 
 $`package:MarkdownReports`
 [1] "superscript_in_plots" "wplot_save_this"      "ww.dev.copy"         
@@ -58,9 +47,8 @@ $`package:stats`
 [1] "cor"    "lm"     "median" "rnorm" 
 
 $`package:Stringendo`
-[1] "AddTrailingSlash"     "iprint"               "kollapse"            
-[4] "kpp"                  "kpps"                 "percentage_formatter"
-[7] "ppp"                  "RemoveDoubleSlash"    "ww.FnP_parser"       
+[1] "iprint"               "kollapse"             "kpps"                 "percentage_formatter"
+[5] "ppp"                 
 
 $`package:utils`
 [1] "capture.output"  "globalVariables"
@@ -69,7 +57,6 @@ c("graphics", "base")
 character(0)
 base
 CodeAndRoll2
-devtools
 graphics
 grDevices
 MarkdownHelpers
